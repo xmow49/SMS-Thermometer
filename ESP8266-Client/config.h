8d8e76e623a4748d0dc88c8d015c7b553a69389a
Xmow <47485034+xmow49@users.noreply.github.com>
2020-07-16 19:53:01 +0200

    V1.5 (07/2020)

--- ESP8266-Client/config.h
+++ ESP8266-Client/config.h
@@ -1,8 +1,9 @@
-static uint8_t PEER[] {0x84, 0x0D, 0x8E, 0xD2, 0x31, 0xE4};
+static uint8_t PEER[] {0x84, 0x0D, 0x8E, 0xD2, 0x31, 0xE4}; //MAC Adress of Server
 
-#define BATTERYPIN A0
-#define DHTPIN 13
-#define DHTTYPE DHT22
-DHT dht(DHTPIN, DHTTYPE);
+#define BATTERYPIN A0 //Analog pin of battery
+#define DHTPIN 13 //pin of DHT22
+#define DHTTYPE DHT22 //DHT type
 
-#define timeDeepSleep 1 // in hour
+DHT dht(DHTPIN, DHTTYPE); //define DHT
+
+#define timeDeepSleep 1 //Interval to send data in hour
